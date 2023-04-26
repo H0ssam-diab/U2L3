893125eb7141377082c31d7c3cd72908d505bac0
Hossam Diab <sam@gmail.com>
2023-04-26 05:04:59 +0200

    my first commit in this lesson

---
+++ src/traiing.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 19, 2023
+ *      Author: Hossam Diab
+ */
+
+
+#include "stdio.h"
+
+int main()
+{
+
+
+	unsigned short  i = 0xFFFF;
+	while (i++!=0) printf("%d",++i);
+
+
+
+
+
+
+}
